a6409a8bab1bbf5ac705b89b3f0ddd2123acd5ce
hashrock <hashrock@users.noreply.github.com>
2019-12-19 18:44:00 +0900

    upd

--- App.vue
+++ App.vue
@@ -1,36 +1,41 @@
 <template>
   <div>
-    <svg width=320 height=240 viewBox="0 0 320 240">
-      <!-- <defs>
+    <svg width="320" height="240" viewBox="0 0 320 240">
+      <defs>
         <symbol id="chip">
-          <image href="./chip.png"></image>
+          <image :xlink:href="chip" x="0" y="0" height="16" width="16" />
         </symbol>
       </defs>
-      <use fill="#db8c81" x="16" y="16" width="16" height="16" xlink:href="#chip"></use> -->
-      <image
-        xlink:href="./chip.png" x=0 y=0 height=16 width=16></image>
+      <use x="0" y="0" xlink:href="#chip" />
+      <use x="16" y="0" xlink:href="#chip" />
+      <use x="0" y="16" xlink:href="#chip" />
     </svg>
   </div>
 </template>
 <script>
+const asset01 =
+  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAGdJREFUOI2l07kRgDAMBdEVQx1uDGqAWtyDG4NGIPNw+JBk5W9/JAEuBm4GWLbDDFMMiAiTZzXFwLqfAPbAE5sDX2wKlLA6UMOqQAt3Az3cDGhwNaDFxYAF/wJW/Ap4cA54MYAw+M43+rMsH0nhAQwAAAAASUVORK5CYII=";
+
 export default {
   data() {
-    return {};
+    return {
+      chip: asset01
+    };
   }
 };
 </script>
 <style>
-svg{
+svg {
   background: white;
   -ms-interpolation-mode: nearest-neighbor;
   image-rendering: pixelated;
   width: 640px;
   height: 480px;
 }
-body{
+body {
   background: #eee;
 }
-image{
+image {
   shape-rendering: crispEdges;
 }
 </style>
